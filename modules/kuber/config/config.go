44268d86e7b566ee7d0d8ec6458c4cec8e50b89b
slifer <dara51php@gmail.com>
2017-09-23 09:48:05 +0330

    update vendor

---
+++ modules/kuber/config/config.go
@@ -0,0 +1,15 @@
+package kcfg
+
+// AllNodes all node const
+var AllNodes = map[string]string{
+	"kube-1.clickyab.ae":  "5.39.69.64",
+	"kube-2.clickyab.ae":  "5.39.74.106",
+	"kube-3.clickyab.ae":  "5.39.74.212",
+	"kube-4.clickyab.ae":  "5.39.75.23",
+	"kube-5.clickyab.ae":  "5.135.141.107",
+	"kube-6.clickyab.ae":  "178.33.122.133",
+	"kube-7.clickyab.ae":  "5.39.69.166",
+	"kube-8.clickyab.ae":  "178.33.229.157",
+	"kube-9.clickyab.ae":  "178.33.229.32",
+	"kube-10.clickyab.ae": "176.31.232.98",
+}
